bb660d170fc1d77735feb816734f23f1914d0614
janelle mellor <janellemellor@gmail.com>
2020-04-29 18:16:03 -0700

    add mock for service function

---
+++ src/services/__mocks__/airbenderApiMock.js
@@ -0,0 +1,9 @@
+export const fetchAirbenderCharacters = () => {
+  return Promise.resolve({
+    name: 'Afiko',
+    affiliation: 'Fire Nation', 
+    image: 'https://vignette.wikia.nocookie.net/avatar/images/2/24/Afiko.png/revision/latest?cb=20121121024128', 
+    allies: 'Fire Nation', 
+    enemies: 'Aang'
+  });
+};
