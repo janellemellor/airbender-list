233fef554a8efbc8d04ef3793d94263c580d09dd
janelle mellor <janellemellor@gmail.com>
2020-05-03 10:33:09 -0700

    map characters component

--- src/components/Characters/Characters.jsx
+++ src/components/Characters/Characters.jsx
@@ -2,11 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Character from './Character.jsx';
 
-const Characters = ({ characters }) => (
-  <>
-  </>
-    
-);
+const Characters = ({ characters }) => {
+  const allCharacters = characters.map(character => (
+    <li key={character.id}>
+      <Character />  
+    </li>
+  ));
+
+  return (
+    <ul>
+      {allCharacters}    
+    </ul>
+  ); 
+};
 
 Characters.propTypes = {
   characters: PropTypes.arrayOf(PropTypes.shape({
