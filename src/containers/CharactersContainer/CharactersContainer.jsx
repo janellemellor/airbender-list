facfcbd7edaadba06fef1d936bb26edf31749195
janelle mellor <janellemellor@gmail.com>
2020-05-03 10:49:43 -0700

    set up characters container page

---
+++ src/containers/CharactersContainer/CharactersContainer.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import Characters from '../../components/Characters/Character.jsx';
+import { fetchAirbenderCharacters } from '../../services/airbenderApi.js';
+
+const CharactersContainer = () => {
+
+
+};
+
+export default CharactersContainer;
