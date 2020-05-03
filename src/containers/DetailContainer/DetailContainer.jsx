5c0db619052442ea6844b1c1460bfbc889524652
janelle mellor <janellemellor@gmail.com>
2020-05-03 12:34:00 -0700

    add state and useEffect to fetch and store character info to detail

--- src/containers/DetailContainer/DetailContainer.jsx
+++ src/containers/DetailContainer/DetailContainer.jsx
@@ -1,17 +1,23 @@
 import React, { useState, useEffect } from 'react';
+import { useRouteMatch } from 'react-router-dom';
 import Detail from '../../components/Detail/Detail.jsx';
-
+import fetchCharacterById from '../../services/airbenderApi.js';
 
 const DetailContainer = () => {
-  const [characters, setCharacters] = useState([]);
+  const [character, setCharacter] = useState({});
+
+  const match = useRouteMatch('/character/:id');
 
   useEffect(() => {
-
+    fetchCharacterById(match.params.id)
+      .then(character => setCharacter(character));
   }, []);
 
   return (
-      
-  )
+    <>
+      <Detail {...character} />
+    </>
+  );
 };
 
 export default DetailContainer;
