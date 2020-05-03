d4e7d0c9ade37509c3be05ab4094fcd0620f60de
janelle mellor <janellemellor@gmail.com>
2020-05-03 10:26:36 -0700

    set up characters

---
+++ src/components/Characters/Characters.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Character from './Character.jsx';
+
+const Characters = ({ characters }) => (
+  <>
+  </>
+    
+);
+
+Characters.propTypes = {
+  characters: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    affiliation: PropTypes.string.isRequired
+  })).isRequired
+};
+
+export default Characters;
