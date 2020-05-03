9a9c6b6d254ea69e9a0b552ac9bf0766e37f5a5c
janelle mellor <janellemellor@gmail.com>
2020-05-03 12:07:28 -0700

    add detail component

---
+++ src/components/Detail/Detail.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Detail = ({ image, name, affiliation, allies, enemies }) => (
+  <>
+    <img src={image} alt={name} />
+    <h3>{name}</h3>
+    <p>{affiliation}</p>
+    <p>Allies:{allies}</p>
+    <p>Enemies:{enemies}</p>
+  </>
+);
+
+Detail.propTypes = {
+  image: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  affiliation: PropTypes.string.isRequired,
+  allies: PropTypes.array.isRequired,
+  enemies: PropTypes.array.isRequired
+};
+
+export default Detail;
