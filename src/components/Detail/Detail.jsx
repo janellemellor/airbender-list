016c6301b439784fca17eb89116f3117a07f1985
janelle mellor <janellemellor@gmail.com>
2020-05-03 15:29:11 -0700

    add space to detail page

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -6,8 +6,8 @@ const Detail = ({ image, name, affiliation, allies, enemies }) => (
     <img src={image} alt={name} />
     <h3>{name}</h3>
     <p>{affiliation}</p>
-    <p>Allies:{allies}</p>
-    <p>Enemies:{enemies}</p>
+    <p>Allies: {allies}</p>
+    <p>Enemies: {enemies}</p>
   </>
 );
 
