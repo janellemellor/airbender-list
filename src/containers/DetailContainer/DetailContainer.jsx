d67a6dd8549d9bd1e915c77b591a96356774b870
janelle mellor <janellemellor@gmail.com>
2020-05-03 12:23:12 -0700

    set up Detail container

---
+++ src/containers/DetailContainer/DetailContainer.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import Detail from '../../components/Detail/Detail.jsx';
+
+
+const DetailContainer = () => {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+
+  }, []);
+
+  return (
+      
+  )
+};
+
+export default DetailContainer;
+
